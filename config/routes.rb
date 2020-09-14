9c51b10dcf2fe00c519694587e73ec74966e65c2
Dmitry Kilin <coding@kilin.biz>
2020-09-14 16:16:13 +0300

    created Staffs controller and controller index view needed for study experiments

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :staffs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#index'
+
+  resources :staffs, only: :index
 end
